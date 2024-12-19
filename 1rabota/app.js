12df139d21ec17a666b458efca1be015bd46bc81
fira zem fira <144517701+Zemfira-B@users.noreply.github.com>
2024-12-19 19:00:05 +0500

    Delete 1rabota directory

--- 1rabota/app.js
+++
@@ -1,9 +0,0 @@
-const ball = document.querySelector(".toggle-ball");
-const items = document.querySelectorAll(".container,.films-list-title,.navbar-container,.sidebar,.left-menu-icon,.toggle");
-
-ball.addEventListener("click", ()=>{
-    items.forEach(items=>{
-        items.classList.toggle("active")
-    })
-    ball.classList.toggle("active")
-})
